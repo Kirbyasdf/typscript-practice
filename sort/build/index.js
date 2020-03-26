01f8e17cd72f5662d7f7cc6fa094727e89e7d3ae
Kirbyasdf <kirbyAugustus@gmail.com>
2020-03-26 17:31:38 -0400

    type guards

--- sort/build/index.js
+++ sort/build/index.js
@@ -4,8 +4,19 @@ var Sorter = /** @class */ (function () {
         this.collection = collection;
         this.collection = collection;
     }
-    Sorter.prototype.sort = function () { };
+    Sorter.prototype.sort = function () {
+        this.collection.sort(function (n1, n2) {
+            if (n1 > n2) {
+                return 1;
+            }
+            if (n1 < n2) {
+                return -1;
+            }
+            return 0;
+        });
+    };
     return Sorter;
 }());
-var sorter = new Sorter([3, 6, 2, 7]).sort();
+var sorter = new Sorter(["a", "c", "d"]);
+sorter.sort();
 console.log(sorter.collection);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,10 +1,23 @@
 class Sorter {
-  constructor(public collection: number[]) {
+  constructor(public collection: number[] | string[]) {
     this.collection = collection;
   }
-  sort(): void {}
+  sort(): void {
+    this.collection.sort((n1: number | string, n2: number | string) => {
+      if (n1 > n2) {
+        return 1;
+      }
+
+      if (n1 < n2) {
+        return -1;
+      }
+
+      return 0;
+    });
+  }
 }
 
-const sorter = new Sorter([3, 6, 2, 7]).sort();
+const sorter = new Sorter(["a", "c", "d"]);
+sorter.sort();
 
 console.log(sorter.collection);
